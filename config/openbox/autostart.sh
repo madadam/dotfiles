5c89013a47dcc4abef8492bd57daedd650f83f62
Adam Cigánek <adam.ciganek@gmail.com>
2010-10-13 21:52:53 +0200

    Sets background before calling global openbox autostart

--- config/openbox/autostart.sh
+++ config/openbox/autostart.sh
@@ -1,13 +1,13 @@
-. $GLOBALAUTOSTART
+# Background
+nitrogen --restore
 
 # Fool xdg-* tools to think I'm running kde
 export DE=kde
 
+. $GLOBALAUTOSTART
+
 # Set keyboard repeat rate
 xset r rate 333 30
-
-# Background
-nitrogen --restore
 
 # Composition
 xcompmgr -n &
